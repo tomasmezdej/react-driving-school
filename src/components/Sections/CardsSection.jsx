09799b47015e49e198e1e0c14fbdd6e4c919ba64
Tomáš Meždej <tmezdej@gmail.com>
2023-02-07 16:53:23 +0100

    animations

--- src/components/Sections/CardsSection.jsx
+++ src/components/Sections/CardsSection.jsx
@@ -7,6 +7,8 @@ import { useState } from "react";
 const CardsSection = () => {
 
   const [scrolledPast, setScrolledPast] = useState(false)
+  const [initiallyHidden, setInitiallyHidden] = useState(true)
+
 
   window.addEventListener("scroll", function() {
     var elementTarget = document.getElementById("INFO_CARD_FIRTS");
@@ -14,6 +16,7 @@ const CardsSection = () => {
     if (elementTarget.offsetTop - window.scrollY < 600) {
       if (!scrolledPast) {
         setScrolledPast(true)
+        setInitiallyHidden(false)
       }
     } else {
       if (scrolledPast) {
@@ -26,7 +29,7 @@ const CardsSection = () => {
     <>
       <div
         id="INFO_CARD_FIRTS"
-        className={`col-12 col-sm-12 col-md-12 col-lg-6 d-flex justify-content-center ${scrolledPast ? "slide-from-left" : "hidden-from-left"} `}
+        className={`col-12 col-sm-12 col-md-12 col-lg-6 d-flex justify-content-center ${scrolledPast ? "slide-from-left" : (initiallyHidden) ? "initially-hidden-left" : "hidden-from-left"} `}
       >
         <InfoCard
           info={firstCard}
@@ -34,7 +37,7 @@ const CardsSection = () => {
       </div>
       <div
         id="INFO_CARD_SECOND"
-        className={`col-12 col-sm-12 col-md-12 col-lg-6 d-flex justify-content-center  ${scrolledPast ? "slide-from-right" : "hidden-from-right"}`}
+        className={`col-12 col-sm-12 col-md-12 col-lg-6 d-flex justify-content-center  ${scrolledPast ? "slide-from-right" : (initiallyHidden) ? "initially-hidden-right" : "hidden-from-right"}`}
       >
         <InfoCard
           info={secondCard}
